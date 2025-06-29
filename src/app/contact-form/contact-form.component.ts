188c2052013782a8fdab288ff66c8ec1601c0256
abdelshafei <ar.elshafei@gmail.com>
2025-06-28 23:50:44 -0400

    Setting up contact form netlify notifications

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, Output, EventEmitter, signal } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { merge } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
@@ -24,6 +24,10 @@ export class ContactFormComponent {
 
   errorMessage = signal('');
 
+  subMessage = signal('');
+  private loadingDots = 0;
+  private loadingInterval: any;
+
   constructor() {
     merge(this.email.statusChanges, this.email.valueChanges)
       .pipe(takeUntilDestroyed())
@@ -40,15 +44,58 @@ export class ContactFormComponent {
     }
   }
 
-  onSubmit(event: Event) {
+  startLoadingAnimation() {
+    this.loadingDots = 0;
+    this.loadingInterval = setInterval(() => {
+      this.loadingDots = (this.loadingDots + 1) % 4;
+      const dots = '.'.repeat(this.loadingDots);
+      this.subMessage.set(`Sending${dots}`);
+    }, 300);
+  }
+
+  stopLoadingAnimation() {
+    clearInterval(this.loadingInterval);
+    this.loadingInterval = null;
+  }
+
+  async onSubmit(event: Event) {
     event.preventDefault(); 
 
     this.isSending = true;
 
-    
+    const formData = new FormData();
+    formData.append('form-name', 'contact');
+    formData.append('name', this.name.value!);
+    formData.append('email', this.email.value!);
+    formData.append('msg', this.msg.value!);
+
+    this.startLoadingAnimation();
+
+    try {
+      await fetch('/', {
+        method: 'POST',
+        body: formData
+      });
+
+      this.subMessage.set('Sent!')
+    } catch (err) {
+      this.subMessage.set('Failed to send.')
+    }
+
+    this.stopLoadingAnimation();
+
+    await delay(2000);
+
+    this.isSending = false;
+    this.close.emit();
   }
 
   onCloseClick() {
     this.close.emit();
   }
 }
+
+function delay(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
